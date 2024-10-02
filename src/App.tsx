af0a722c84b3727083d9dbc63bcffbaa92df5f4d
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-10-02 18:27:23 -0400

    game

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-import {
-  BrowserRouter as Router,
-  Route,
-} from "react-router-dom";
-import StartScreen from "./pages/startscreen";
 import GameScreen from "./pages/gamescreen";
-import ResultScreen from "./pages/resultscreen";
+
 
 function App() {
-  return(
-  <Router>
-        <Route path="/">
-          <StartScreen/>
-        </Route>
-        <Route path="/game">
-          <GameScreen/>
-        </Route>
-        <Route path="/result">
-          <ResultScreen/>
-        </Route>
-  </Router>
-  )
+  return( 
+      <GameScreen/>
+);
+ 
+
   
 }
 
--- src/pages/gamescreen.tsx
+++ src/pages/gamescreen.tsx
@@ -1,5 +1,53 @@
+import { MouseEventHandler, useEffect, useState } from "react";
+import { type boardIdx, type Game } from "../game";
+import { move } from "../game";
+
+
 function GameScreen(){
-    return(<div></div>);
+    const initialGameState:Game = {
+        currPlayer:'x',
+        boardState: ['', '', '', '', '', '', '', '', ''],
+        winState: 'undecided'
+    };
+    const [game,setGame] = useState<Game>(initialGameState);
+
+
+    useEffect(()=>{
+        if(game.winState === 'win'){
+            const winner = game.currPlayer === 'x'?'o':'x';
+            alert(winner+' Won!');
+            setGame(initialGameState);
+        }
+        if(game.winState === 'tie'){
+            alert("It's a tie");
+            setGame(initialGameState);
+        }},[game]
+
+    )
+
+
+    function onCellClick(idx:boardIdx): MouseEventHandler {
+        return () => {
+            const newGame = move(game, idx);
+            setGame(newGame);
+          }
+    }
+
+    return(
+        <div>
+            <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gridTemplateRows: 'repeat(3, 1fr)', width: '300px', height: '300px' }}>
+                {game.boardState.map((cell,index)=>{
+                    const onClickHandler = onCellClick(index as boardIdx);
+                    return(
+                        <div onClick={onClickHandler} key={index} style={{ border: '1px solid #ccc', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '24px' }}>
+                            {cell}
+                        </div>  
+                    )
+                })
+                }
+            </div>
+        </div>
+    );
 
 };
 
